a3bc47a9a26837b907fbe006b297b7559c7c577e
GerardSzymanski94 <gerard.szymanski1@gmail.com>
2022-11-02 16:18:05 +0100

    Uruchomienie UserSeeder w db:seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        UserSeeder::class;
+        $this->call([
+            UserSeeder::class,
+        ]);
     }
 }
